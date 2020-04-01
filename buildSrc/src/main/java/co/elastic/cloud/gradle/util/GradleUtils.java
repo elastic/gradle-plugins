f593057b698ee1334a34e8b65d5734a43502f54c
Francois Teychene <francois.teychene@elastic.co>
2020-04-01 16:54:59 +0200

    Gradle : add retry to tests only if no filter is declared (#54282)
    * feat: Add retry to tests only if no filter is declared (command line of IntelliJ)

---
+++ buildSrc/src/main/java/co/elastic/cloud/gradle/util/GradleUtils.java
@@ -0,0 +1,29 @@
+package co.elastic.cloud.gradle.util;
+
+import org.gradle.api.internal.tasks.testing.filter.DefaultTestFilter;
+import org.gradle.api.specs.Spec;
+import org.gradle.api.tasks.testing.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GradleUtils {
+
+    public static boolean areTestsFiltered(Test task) {
+        return Optional.ofNullable(task.getFilter())
+                .filter(it -> it instanceof DefaultTestFilter)
+                .map(it -> (DefaultTestFilter) it)
+                .map(it -> {
+                    Set<String> filters = new HashSet<>();
+                    filters.addAll(Optional.ofNullable(it.getCommandLineIncludePatterns()).orElse(Collections.emptySet()));
+                    filters.addAll(Optional.ofNullable(it.getIncludePatterns()).orElse(Collections.emptySet()));
+                    return filters;
+                })
+                .map(filters -> !filters.isEmpty())
+                .orElse(false);
+    }
+}
