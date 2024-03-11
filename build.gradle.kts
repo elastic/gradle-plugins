e75821d6925ef7ae93ea4a61bd87ba3f462775d8
Alpar Torok <alpar.torok@elastic.co>
2024-03-11 12:30:36 +0200

    Add license headers

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,3 +45,50 @@ tasks.register("concatenateLicenseReports") {
         }
     }
 }
+
+val licenseHeader = """
+   /*
+    * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+    * or more contributor license agreements. See the NOTICE file distributed with
+    * this work for additional information regarding copyright
+    * ownership. Elasticsearch B.V. licenses this file to you under
+    * the Apache License, Version 2.0 (the "License"); you may
+    * not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *	http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing,
+    * software distributed under the License is distributed on an
+    * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    * KIND, either express or implied.  See the License for the
+    * specific language governing permissions and limitations
+    * under the License.
+    */
+""".trimIndent()
+
+tasks.register("addLicenseHeader") {
+    group = "build"
+    description = "Adds license header to Java files"
+
+    doLast {
+        addLicenseHeaderRecursively(projectDir)
+    }
+}
+
+fun addLicenseHeaderRecursively(directory: File) {
+    directory.listFiles()?.forEach { file ->
+        if (file.isDirectory) {
+            addLicenseHeaderRecursively(file)
+        } else if (file.isFile && file.name.endsWith(".java")) {
+            addLicenseHeader(file)
+        }
+    }
+}
+
+fun addLicenseHeader(file: File) {
+    val content = file.readText()
+    if (!content.startsWith("/*")) {
+        file.writeText("$licenseHeader\n$content")
+    }
+}
