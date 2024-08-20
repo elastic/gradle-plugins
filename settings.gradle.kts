144494ab65e373b3e23af29dc16ec0801f8c91cd
Isaac Karrer <isaac.karrer@elastic.co>
2024-08-19 20:42:25 -0500

    restore provider

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,10 @@
+import co.elastic.gradle.vault.VaultExtension
 import java.io.File
 
 plugins {
+    id("co.elastic.vault").version(File("version-released").readText().trim())
     id("com.gradle.enterprise").version("3.9")
     id("co.elastic.elastic-conventions").version(File("version-released").readText().trim())
-    id("co.elastic.vault").version(File("version-released").readText().trim())
 }
 
 include("libs")
